b2221375b24990ecf55d2dfc4981732669ebdba9
ltn290999 <53668823+ltn290999@users.noreply.github.com>
2020-08-13 17:45:38 +0700

    new version

---
+++ ExoVisix-master/ExoVisix-master/ExoVisix-master/ExoVisix-master/src/application/Database.java
@@ -0,0 +1,185 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+class Database {
+	public int code;
+
+	public String fullName;
+	public String className;
+	public int idMonHoc;
+
+	public final String Database_name = "ghosteye";
+	public final String Database_user = "root";
+	public final String Database_pass = "";
+
+	public Connection con;
+
+	public boolean init() throws SQLException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			try {
+				this.con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + Database_name, Database_user,
+						Database_pass);
+			} catch (SQLException e) {
+
+				System.out.println("Error: Database Connection Failed ! Please check the connection Setting");
+
+				return false;
+
+			}
+
+		} catch (ClassNotFoundException e) {
+
+			e.printStackTrace();
+
+			return false;
+		}
+
+		return true;
+	}
+
+	public void insert() {
+		String sql = "INSERT INTO face_bio (id,code,fullName,className,monHoc) VALUES (?, ?, ?, ?,?)";
+
+		PreparedStatement statement = null;
+		try {
+			statement = con.prepareStatement(sql);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			statement.setInt(1, 0);
+			statement.setInt(2, this.code);
+			statement.setString(3, this.fullName);
+			statement.setString(4, this.className);
+			statement.setInt(5, 2);
+			int rowsInserted = statement.executeUpdate();
+			if (rowsInserted > 0) {
+				System.out.println("A new face data was inserted successfully!");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public int getCode(int code) {
+		int result = 0;
+		try {
+
+			String sql = "select * from face_bio where code=" + code;
+
+			Statement s = con.createStatement();
+
+			ResultSet rs = s.executeQuery(sql);
+
+			if (rs.next()) {
+				result = rs.getInt("id");
+			}
+
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return result;
+	}
+
+	public ArrayList<String> getUser(int inCode) throws SQLException {
+
+		ArrayList<String> user = new ArrayList<String>();
+
+		try {
+
+			Database app = new Database();
+
+			String sql = "select * from face_bio where code=" + inCode + " limit 1";
+
+			Statement s = con.createStatement();
+
+			ResultSet rs = s.executeQuery(sql);
+
+			while (rs.next()) {
+
+				/*
+				 * app.setCode(rs.getInt(2)); app.setFname(rs.getString(3));
+				 * app.setLname(rs.getString(4)); app.setReg(rs.getInt(5));
+				 * app.setAge(rs.getInt(6)); app.setSec(rs.getString(7));
+				 */
+
+				user.add(0, Integer.toString(rs.getInt(2)));
+				user.add(1, rs.getString(3));
+				user.add(2, rs.getString(4));
+				user.add(3, Integer.toString(rs.getInt(5)));
+
+				/*
+				 * System.out.println(app.getCode()); System.out.println(app.getFname());
+				 * System.out.println(app.getLname()); System.out.println(app.getReg());
+				 * System.out.println(app.getAge()); System.out.println(app.getSec());
+				 */
+
+				// nString="Name:" + rs.getString(3)+" "+rs.getString(4) +
+				// "\nReg:" + app.getReg() +"\nAge:"+app.getAge() +"\nSection:"
+				// +app.getSec() ;
+
+				// System.out.println(nString);
+			}
+
+			con.close(); // closing connection
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return user;
+	}
+
+	public void db_close() throws SQLException {
+		try {
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public int getIdMonHoc() {
+		return idMonHoc;
+	}
+
+	public void setIdMonHoc(int idMonHoc) {
+		this.idMonHoc = idMonHoc;
+	}
+
+}
---
+++ ExoVisix-master/ExoVisix-master/ExoVisix-master/ExoVisix-master/src/application/SampleController.java
@@ -0,0 +1,395 @@
+package application;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
+import javafx.scene.text.TextFlow;
+import javafx.util.Duration;
+
+public class SampleController {
+
+	// **********************************************************************************************
+	// Mention The file location path where the face will be saved & retrieved
+
+	public String filePath = "./faces";
+
+	// **********************************************************************************************
+	@FXML
+	private Button startCam;
+	@FXML
+	private Button stopBtn;
+
+	@FXML
+	private Button saveBtn;
+
+	@FXML
+	private Button recogniseBtn;
+	@FXML
+	private Button stopRecBtn;
+	@FXML
+	private ImageView frame;
+	@FXML
+	private ImageView motionView;
+	@FXML
+	private AnchorPane pdPane;
+	@FXML
+	private TitledPane dataPane;
+	@FXML
+	private TextField fullName;
+	@FXML
+	private TextField className;
+	@FXML
+	private TextField code;
+
+	@FXML
+	public ListView<String> logList;
+	@FXML
+	public ListView<String> output;
+	@FXML
+	public ProgressIndicator pb;
+	@FXML
+	public Label savedLabel;
+	@FXML
+	public Label warning;
+	@FXML
+	public Label title;
+	@FXML
+	public TilePane tile;
+	@FXML
+	public TextFlow ocr;
+	@FXML
+	public Label time;
+//**********************************************************************************************
+	FaceDetector faceDetect = new FaceDetector(); // Creating Face detector object
+	Database database = new Database(); // Creating Database object
+
+	ArrayList<String> user = new ArrayList<String>();
+	ImageView imageView1;
+
+	public static ObservableList<String> event = FXCollections.observableArrayList();
+	public static ObservableList<String> outEvent = FXCollections.observableArrayList();
+
+	public boolean enabled = false;
+	public boolean isDBready = false;
+
+	// **********************************************************************************************
+	public void putOnLog(String data) {
+
+		Instant now = Instant.now();
+
+		String logs = now.toString() + ":\n" + data;
+
+		event.add(logs);
+
+		logList.setItems(event);
+
+	}
+
+	@FXML
+	protected void startCamera() throws SQLException {
+
+		new Thread(() -> {
+			javafx.application.Platform.runLater(new Runnable() {
+
+				@Override
+				public void run() {
+					Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+						LocalTime currentTime = LocalTime.now();
+						time.setText(
+								currentTime.getHour() + ":" + currentTime.getMinute() + ":" + currentTime.getSecond());
+					}), new KeyFrame(Duration.seconds(1)));
+					clock.setCycleCount(Animation.INDEFINITE);
+					clock.play();
+				}
+			});
+
+		}).start();
+
+		// *******************************************************************************************
+		// initializing objects from start camera button event
+		faceDetect.init();
+
+		faceDetect.setFrame(frame);
+
+		faceDetect.start();
+
+		if (!database.init()) {
+
+			putOnLog("Error: Database Connection Failed ! ");
+
+		} else {
+			isDBready = true;
+			putOnLog("Success: Database Connection Succesful ! ");
+		}
+
+		// *******************************************************************************************
+		// Activating other buttons
+		startCam.setVisible(false);
+
+		stopBtn.setVisible(true);
+		saveBtn.setDisable(false);
+
+		if (isDBready) {
+			recogniseBtn.setDisable(false);
+		}
+
+		dataPane.setDisable(false);
+		// shapeBtn.setDisable(false);
+
+		if (stopRecBtn.isDisable()) {
+			stopRecBtn.setDisable(false);
+		}
+		// *******************************************************************************************
+
+		tile.setPadding(new Insets(15, 15, 55, 15));
+		tile.setHgap(30);
+
+		// **********************************************************************************************
+		// Picture Gallary
+
+		String path = filePath;
+
+		File folder = new File(path);
+		File[] listOfFiles = folder.listFiles();
+
+		// Image reader from the mentioned folder
+		for (final File file : listOfFiles) {
+
+			imageView1 = createImageView(file);
+			tile.getChildren().addAll(imageView1);
+		}
+		putOnLog(" Real Time WebCam Stream Started !");
+
+		// **********************************************************************************************
+	}
+
+	int count = 0;
+
+	@FXML
+	protected void faceRecognise() {
+
+		faceDetect.setIsRecFace(true);
+		// printOutput(faceDetect.getOutput());
+
+		recogniseBtn.setText("Get Face Data");
+
+		// Getting detected faces
+		user = faceDetect.getOutput();
+
+		if (count > 0) {
+
+			// Retrieved data will be shown in Fetched Data pane
+			String t = "********* Face Data: " + user.get(1) + " " + user.get(2) + " *********";
+
+			outEvent.add(t);
+
+			String n1 = "First Name\t\t:\t" + user.get(1);
+
+			outEvent.add(n1);
+
+			output.setItems(outEvent);
+
+			String n2 = "Last Name\t\t:\t" + user.get(2);
+
+			outEvent.add(n2);
+
+			output.setItems(outEvent);
+
+			String fc = "Face Code\t\t:\t" + user.get(0);
+
+			outEvent.add(fc);
+
+			output.setItems(outEvent);
+
+			String r = "Reg no\t\t\t:\t" + user.get(3);
+
+			outEvent.add(r);
+
+			output.setItems(outEvent);
+
+			String a = "Age \t\t\t\t:\t" + user.get(4);
+
+			outEvent.add(a);
+
+			output.setItems(outEvent);
+			String s = "Section\t\t\t:\t" + user.get(5);
+
+			outEvent.add(s);
+
+			output.setItems(outEvent);
+
+		}
+
+		count++;
+
+		putOnLog("Face Recognition Activated !");
+
+		stopRecBtn.setDisable(false);
+
+	}
+
+	@FXML
+	protected void stopRecognise() {
+
+		faceDetect.setIsRecFace(false);
+		faceDetect.clearOutput();
+
+		this.user.clear();
+
+		recogniseBtn.setText("Recognise Face");
+
+		stopRecBtn.setDisable(true);
+
+		putOnLog("Face Recognition Deactivated !");
+
+	}
+
+	@FXML
+	protected void startMotion() {
+
+		faceDetect.setMotion(true);
+		putOnLog("motion Detector Activated !");
+
+	}
+
+	@FXML
+	protected void saveFace() throws SQLException {
+
+		// Input Validation
+
+		if (fullName.getText().trim().isEmpty() || className.getText().trim().isEmpty()
+				|| code.getText().trim().isEmpty()) {
+			new Thread(() -> {
+
+				try {
+					warning.setVisible(true);
+
+					Thread.sleep(2000);
+
+					warning.setVisible(false);
+
+				} catch (InterruptedException ex) {
+				}
+
+			}).start();
+
+		} else {
+			// Progressbar
+			pb.setVisible(true);
+
+			savedLabel.setVisible(true);
+
+			new Thread(() -> {
+
+				try {
+
+					faceDetect.setFullName(fullName.getText());
+					faceDetect.setClassName(className.getText());
+					faceDetect.setCode(Integer.parseInt(code.getText()));
+
+					database.setFullName(fullName.getText());
+					database.setClassName(className.getText());
+					database.setCode(Integer.parseInt(code.getText()));
+					if (database.getCode(Integer.parseInt(code.getText())) == 0) {
+						database.insert();
+					}
+
+					javafx.application.Platform.runLater(new Runnable() {
+
+						@Override
+						public void run() {
+							pb.setProgress(100);
+						}
+					});
+					savedLabel.setVisible(true);
+					Thread.sleep(2000);
+
+					javafx.application.Platform.runLater(new Runnable() {
+
+						@Override
+						public void run() {
+							pb.setVisible(false);
+						}
+					});
+					javafx.application.Platform.runLater(new Runnable() {
+
+						@Override
+						public void run() {
+							savedLabel.setVisible(false);
+						}
+					});
+
+				} catch (InterruptedException ex) {
+				}
+
+			}).start();
+
+			faceDetect.setSaveFace(true);
+
+		}
+	}
+
+	@FXML
+	protected void stopCam() throws SQLException {
+
+		faceDetect.stop();
+
+		startCam.setVisible(true);
+		stopBtn.setVisible(false);
+
+		/* this.saveFace=true; */
+
+		putOnLog("Cam Stream Stopped!");
+
+		recogniseBtn.setDisable(true);
+		saveBtn.setDisable(true);
+		dataPane.setDisable(true);
+		stopRecBtn.setDisable(true);
+		database.db_close();
+		putOnLog("Database Connection Closed");
+		isDBready = false;
+	}
+
+	private ImageView createImageView(final File imageFile) {
+
+		try {
+			final Image img = new Image(new FileInputStream(imageFile), 120, 0, true, true);
+			imageView1 = new ImageView(img);
+
+			imageView1.setStyle("-fx-background-color: BLACK");
+			imageView1.setFitHeight(120);
+
+			imageView1.setPreserveRatio(true);
+			imageView1.setSmooth(true);
+			imageView1.setCache(true);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return imageView1;
+	}
+
+}
